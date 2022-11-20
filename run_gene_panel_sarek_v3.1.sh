ad7153c33ab025ea1e87ba0aa180bd41bf2f6f4e
SVZhuk <s.v.zhuk@gmail.com>
2022-11-21 02:26:22 +0300

    edited parameters according to new pipeline

---
+++ run_gene_panel_sarek_v3.1.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Script for variant calling from SureSelect capture experiments
+# Uses nf-core conda env
+# Uses local sarek pipeline
+
+PipelineDir="/home/students/nextflow_dir/pipelines/nf-core-sarek-3.1/workflow/"
+
+InputCsv=$1
+Probes=$2
+Name=$3
+
+echo "Input file used is: $InputCsv."
+echo "Probes used for capture are: $Probes."
+echo "Name of output folder for results is: $Name."
+
+if [[ -z "$InputCsv" ]]; then
+  echo "Input first positional argument: TSV with samples! Exiting..."
+  exit 1
+fi
+
+if [[ -z "$Probes" ]]; then
+  echo "Input second positional argument: bed file with sorted probe intervals! Exiting..."
+  exit 1
+fi
+
+if [[ -z "$Name" ]]; then
+  echo "Input third positional argument: name for results output folder! Exiting..."
+  exit 1
+fi
+
+echo "Activating nf-core conda environment."
+source /mnt/hdd/nextflow_dir/workspace/anaconda3/etc/profile.d/conda.sh
+conda activate nf-core
+
+echo "Currently running shell from $(pwd)"
+
+iGenomes_base="/mnt/hdd/nextflow_dir/workspace/REF/iGenomes/"
+
+nextflow run ${PipelineDir}  \
+    -profile docker \
+    --input ${InputCsv} \
+    --outdir results-${Name} \
+    --genome GATK.GRCh38 \
+    --igenomes_base ${iGenomes_base} \
+    --wes true \
+    --intervals ${Probes} \
+    --tools haplotypecaller,strelka,merge \
+    --max_cpus 80 \
+    --max_memory 180.GB \
+    -resume
